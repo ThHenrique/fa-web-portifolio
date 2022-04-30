7c485412a29b72b6342a6d54a65225fd036cebf7
thhenrique <henriquethiago298@gmail.com>
2022-04-29 21:37:00 -0300

    remove ajax and jquery modules

--- src/pages/portifolio/portifolio.js
+++ src/pages/portifolio/portifolio.js
@@ -1,52 +1,65 @@
-jQuery.githubUser = function (username, callback) {
-  jQuery.getJSON(`https://api.github.com/users/${username}/repos`, callback);
-};
+const loaderContainerEl = window.document.querySelector(".loadercontainer");
+const listContainerEl = window.document.querySelector(".cards-wrap");
 
-jQuery.fn.loadRepositories = function (username) {
-  this.html(
-    /*html */
-    `<div class="loadercontainer">
-      <div id="loading">
-        <div class="lds-ripple">
-          <div></div>
-          <div></div>
-        </div>
-        Carregando...
-      </div>
-    </div>`
-  );
-
-  var target = this;
-  $.githubUser(username, function (data) {
-    var repositories = filterRepositories(data);
-
-    var list = $("<ul/>");
-    target.empty().append(list);
-    $(repositories).each(function () {
-      list.append(
-        /* html */
-        `  
-        <li class="card">
-          <img
-          class="card-img-top"
-          src='https://raw.githubusercontent.com/${username}/${this.name}/${
-          this.default_branch
-        }/.github/cover.png' alt="${this.name}-logo" />
-          <section class="card-body">
-            <h4 class="card-title">${this.name}</h4>
-            <p card="card-text">${this.description ?? ""}</p>
-            <p card="card-text">${this.language ?? ""}</p>
-            <a class="btn btn-outline-dark" href='${
-              this.svn_url
-            }' target='_blank'>Saber mais</a>
-          </section>
-        </li>     
-        `
-      );
+function githubUser(username, callback) {
+  fetch(`https://api.github.com/users/${username}/repos`)
+    .then((response) => response.json())
+    .then((data) => callback(data))
+    .catch(() => {
+      alert("Algo inesperado aconteceu");
+    })
+    .finally(() => {
+      loaderContainerEl.classList.add("hidden");
     });
+}
+
+function loadRepositories() {
+  const username = "thhenrique";
+
+  githubUser(username, function (data) {
+    const repositories = filterRepositories(data);
+
+    const list = document.createElement("ul");
+
+    repositories.forEach((item) => {
+      const card = document.createElement("li");
+      card.classList.add("card");
+
+      const img = document.createElement("img");
+      const coverSrc = `https://raw.githubusercontent.com/${username}/${item.name}/${item.default_branch}/.github/cover.png`;
+
+      img.src = coverSrc;
+      img.classList.add("card-img-top");
+      img.alt = `${item.name}-logo`;
+
+      img.addEventListener("error", () => {
+        img.src = "../../assets/images/cover-default-github.png";
+      });
+      card.appendChild(img);
+
+      const section = document.createElement("section");
+      section.classList.add("card-body");
+
+      section.innerHTML = ` 
+        <h4 class="card-title">${item.name}</h4>
+        <p card="card-text">${item.description ?? ""}</p>
+        <p card="card-text">${item.language ?? ""}</p>
+        <a class="btn btn-outline-dark" href='${
+          item.svn_url
+        }' target='_blank'>Saber mais</a>
+        `;
+
+      card.appendChild(section);
+
+      list.appendChild(card);
+    });
+
+    listContainerEl.appendChild(list);
   });
 
   function filterRepositories(repos) {
     return repos.filter((repo) => !repo.fork);
   }
-};
+}
+
+loadRepositories();
